ef38b14b00bb0ad8f13afd795b60be3eafeaba83
WangYan-Good <61375678+WangYan-Good@users.noreply.github.com>
2025-05-05 02:45:57 -0400

    [Feature][Frontend] update paste link from clipboard (#36)

--- frontend/src/static/js/clipboard.js
+++ frontend/src/static/js/clipboard.js
@@ -1,12 +1,25 @@
-function single_input() {
-    // 点击按钮时获取剪贴板文本并写入输入框
-    // 使用 Clipboard API 获取剪贴板文本
-    navigator.clipboard.readText()
-        .then(function (text) {
-            // 将剪贴板文本写入输入框
-            $('#linkInput').val(text);
-        })
-        .catch(function (error) {
-            console.error('读取剪贴板失败: ', error);
+// 复制到剪贴板
+document.addEventListener('DOMContentLoaded', function () {
+    // 绑定按钮点击事件
+    const pasteButton = document.getElementById('pasteButton');
+    const textarea = document.getElementById('linkInput');
+    const status = document.getElementById('status');
+
+    if (pasteButton) {
+        pasteButton.addEventListener('click', async function () {
+            // 使用 Clipboard API 获取剪贴板文本
+            try {
+                const text = await navigator.clipboard.readText();
+                if (text) {
+                    textarea.value = '';
+                    textarea.value = text;
+                    status.textContent = '读取剪贴板内容成功！'
+                }
+            } catch (err) {
+                // alert("自动读取剪贴板失败，请手动点击粘贴按钮。");
+                status.textContent = '自动读取剪贴板失败，请手动点击粘贴按钮。';
+                console.log('自动读取剪贴板失败:', err);
+            }
         });
-}
+    }
+});
