46cd80ceb2f990b7468f9a452c30de08a322467f
WangYan-Good <WangYan-Good@github.com>
2025-05-26 03:50:17 -0400

    [front] remove paste clipboard listener and use onclick

--- frontend/src/static/js/clipboard.js
+++ frontend/src/static/js/clipboard.js
@@ -1,25 +1,17 @@
-// 复制到剪贴板
-document.addEventListener('DOMContentLoaded', function () {
-    // 绑定按钮点击事件
-    const pasteButton = document.getElementById('pasteButton');
+async function pasteFromClipboard() {
     const textarea = document.getElementById('linkInput');
     const status = document.getElementById('status');
 
-    if (pasteButton) {
-        pasteButton.addEventListener('click', async function () {
-            // 使用 Clipboard API 获取剪贴板文本
-            try {
-                const text = await navigator.clipboard.readText();
-                if (text) {
-                    textarea.value = '';
-                    textarea.value = text;
-                    status.textContent = '读取剪贴板内容成功！'
-                }
-            } catch (err) {
-                // alert("自动读取剪贴板失败，请手动点击粘贴按钮。");
-                status.textContent = '自动读取剪贴板失败，请手动点击粘贴按钮。';
-                console.log('自动读取剪贴板失败:', err);
-            }
-        });
+    // 使用 Clipboard API 获取剪贴板文本
+    try {
+        const text = await navigator.clipboard.readText();
+        if (text) {
+            textarea.value = '';
+            textarea.value = text;
+            status.textContent = '读取剪贴板内容成功！';
+        }
+    } catch (err) {
+        status.textContent = '自动读取剪贴板失败，请手动点击粘贴按钮。';
+        console.error('自动读取剪贴板失败:', err);
     }
-});
+}
