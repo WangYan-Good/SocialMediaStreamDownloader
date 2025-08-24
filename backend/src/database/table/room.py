cf438a0e449f7ac9c26badb5c939742a2f0301be
WangYan-Good <987639823@qq.com>
2025-08-24 13:15:48 +0800

    [feature][implementation] add stream_complete_push_url table and unit test

--- backend/src/database/table/room.py
+++ backend/src/database/table/room.py
@@ -2748,7 +2748,6 @@ class StreamCandidateResolutionTable(SocialMediaStreamDataTable):
   def get_drop_sql_cmd(self) -> str:
     return self.__SQL_DROP_STREAM_CANDIDATE_RESOLUTION_TABLE
 
-
 ##
 ## data.room.stream_url.complete_push_urls
 ##
@@ -2764,7 +2763,79 @@ class StreamCandidateResolutionTable(SocialMediaStreamDataTable):
 ## +-------------------------+-------------------+------+-----+---------+-------+---------------------------------------------+---------------------+
 ##
 class StreamCompletePushUrlTable(SocialMediaStreamDataTable):
-  pass
+##
+## >>=============================== attribute ===============================>>
+##
+  __STREAM_COMPLETE_PUSH_URL_TABLE_NAME       = "stream_complete_push_url"
+  __STREAM_COMPLETE_PUSH_URL_TABLE_HEADER     = ['now', 'platform', 'room_id', 'stream_id', 'complete_push_url_index', 'complete_push_url']
+  __STREAM_COMPLETE_PUSH_URL_TABLE_PRI_KEY    = ['now', 'platform', 'room_id', 'stream_id', 'complete_push_url_index']
+  __STREAM_COMPLETE_PUSH_URL_TABLE_TUPLE      = {item:None for item in __STREAM_COMPLETE_PUSH_URL_TABLE_HEADER}
+  __SQL_CREATE_STREAM_COMPLETE_PUSH_URL_TABLE = '''
+                                                   CREATE TABLE IF NOT EXISTS {} (
+                                                     now                     timestamp(3) NOT NULL,
+                                                     platform                varchar(20)  NOT NULL,
+                                                     room_id                 varchar(200) NOT NULL,
+                                                     stream_id               varchar(200) NOT NULL,
+                                                     complete_push_url_index tinyint      NOT NULL,
+                                                     complete_push_url       varchar(20)  DEFAULT NULL,
+                                                     PRIMARY KEY (now, platform, room_id, stream_id, complete_push_url_index)
+                                                   )
+                                                   '''.format(__STREAM_COMPLETE_PUSH_URL_TABLE_NAME)
+  __SQL_DROP_STREAM_COMPLETE_PUSH_URL_TABLE   = 'DROP TABLE IF EXISTS {};'.format(__STREAM_COMPLETE_PUSH_URL_TABLE_NAME)
+
+##
+## >>============================= private method =============================>>
+##
+  ##
+  ## singleton mode
+  ##
+  def __new__(cls, *args, **kwargs):
+    return super().__new__(cls, *args, **kwargs)
+
+  ##
+  ## init method
+  ##
+  def __init__(self, db_instance:SocialMediaStreamDataBase = None) -> None:
+    super().__init__(db_instance)
+
+##
+## >>============================= abstract method =============================>>
+##
+  ##
+  ## get table name
+  ##
+  def get_name(self) -> str:
+    return self.__STREAM_COMPLETE_PUSH_URL_TABLE_NAME
+  
+  ##
+  ## get table header
+  ##
+  def get_header(self) -> list:
+    return self.__STREAM_COMPLETE_PUSH_URL_TABLE_HEADER
+
+  ##
+  ## get table tuple
+  ##
+  def get_tuple(self) -> dict:
+    return self.__STREAM_COMPLETE_PUSH_URL_TABLE_TUPLE
+
+  ##
+  ## get table primary key
+  ##
+  def get_pri_key(self) -> list:
+    return self.__STREAM_COMPLETE_PUSH_URL_TABLE_PRI_KEY
+
+  ##
+  ## get SQL command of create table
+  ##
+  def get_create_sql_cmd(self) -> str:
+    return self.__SQL_CREATE_STREAM_COMPLETE_PUSH_URL_TABLE
+
+  ##
+  ## get SQL command of drop table
+  ##
+  def get_drop_sql_cmd(self) -> str:
+    return self.__SQL_DROP_STREAM_COMPLETE_PUSH_URL_TABLE
 
 ##
 ## data.room.stream_url.live_core_sdk_data
--- backend/src/unit_test/database/table/room.py
+++ backend/src/unit_test/database/table/room.py
@@ -30,7 +30,8 @@ from backend.src.database.table.room                                  import Roo
                                                                              RoomTempStateGlobalConditionTable, \
                                                                              RoomRecordTable, \
                                                                              LiveStreamTable, \
-                                                                             StreamCandidateResolutionTable
+                                                                             StreamCandidateResolutionTable, \
+                                                                             StreamCompletePushUrlTable
 from backend.src.base.log                                             import get_logger
 
 ##
@@ -3714,6 +3715,206 @@ def test_get_stream_candidate_resolution_record(db:SocialMediaStreamDataBase = N
     raise e
 
 ##
+## >>================================ stream complete push url table test method ===============================>>
+##
+
+def test_create_stream_complete_push_url_table(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if db is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table
+  ##
+  stream_complete_push_url = StreamCompletePushUrlTable(db_instance=db)
+  stream_complete_push_url.create()
+  return
+
+##
+## test: drop table
+##
+def test_drop_stream_complete_push_url_table(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## drop table
+  ##
+  stream_complete_push_url = StreamCompletePushUrlTable(db_instance=db)
+  stream_complete_push_url.drop()
+  return
+
+##
+## test: check if table exists
+##
+def test_check_stream_complete_push_url_exists(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+
+  stream_complete_push_url = StreamCompletePushUrlTable(db)
+
+  ##
+  ## check if table exists
+  ##
+  if db.is_table_exist(stream_complete_push_url.get_name()):
+    get_logger().info("{} table exists!".format(stream_complete_push_url.get_name()))
+  else:
+    get_logger().info("{} table not exists!".format(stream_complete_push_url.get_name()))
+  return
+
+##
+## test: insert record
+##
+def test_insert_stream_complete_push_url_record(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table if not exists
+  ##
+  stream_complete_push_url = StreamCompletePushUrlTable(db_instance=db)
+
+  ##
+  ## insert a sample record
+  ##
+  sample_record = {
+    'now': dat.fromtimestamp(1740301577026/1000.0),
+    'platform': 'douyin',
+    'room_id': '7411524533301119798',
+    'stream_id': '691500607505433258',
+    'complete_push_url_index': 0
+  }
+  
+  try:
+    stream_complete_push_url.insert_record(sample_record)
+    get_logger().info("sample record inserted successfully")
+  except Exception as e:
+    get_logger().error("failed to insert sample record: {}".format(e))
+    raise e
+
+##
+## test: delete record
+##
+def test_delete_stream_complete_push_url_record(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table if not exists
+  ##
+  stream_complete_push_url = StreamCompletePushUrlTable(db_instance=db)
+
+  ##
+  ## delete a sample record
+  ##
+  sample_record = {
+    'now': dat.fromtimestamp(1740301577026/1000.0),
+    'platform': 'douyin',
+    'room_id': '7411524533301119798',
+    'stream_id': '691500607505433258',
+    'complete_push_url_index': 0
+  }
+  
+  try:
+    stream_complete_push_url.delete_record(sample_record)
+    get_logger().info("sample record deleted successfully")
+  except Exception as e:
+    get_logger().error("failed to delete sample record: {}".format(e))
+    raise e
+
+##
+## test: update record
+##
+def test_update_stream_complete_push_url_record(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table if not exists
+  ##
+  stream_complete_push_url = StreamCompletePushUrlTable(db_instance=db)
+
+  ##
+  ## update a sample record
+  ##
+  sample_record = {
+    'now': dat.fromtimestamp(1740301577026/1000.0),
+    'platform': 'douyin',
+    'room_id': '7411524533301119798',
+    'stream_id': '691500607505433258',
+    'complete_push_url_index': 0,
+    'complete_push_url': 'TBD'
+  }
+  
+  try:
+    stream_complete_push_url.update_record(sample_record)
+    get_logger().info("sample record updated successfully")
+  except Exception as e:
+    get_logger().error("failed to update sample record: {}".format(e))
+    raise e
+
+##
+## test: get record
+##
+def test_get_stream_complete_push_url_record(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table if not exist
+  ##
+  stream_complete_push_url = StreamCompletePushUrlTable(db)
+
+  ##
+  ## get a sample record
+  ##
+  sample_record = {
+    'now': dat.fromtimestamp(1740301577026/1000.0),
+    'platform': 'douyin',
+    'room_id': '7411524533301119798',
+    'stream_id': '691500607505433258',
+    'complete_push_url_index': 0
+  }
+  
+  try:
+    record = stream_complete_push_url.get_record(sample_record)
+    if record:
+      get_logger().info("sample {} record retrieved successfully: \n\t{}".format(stream_complete_push_url.get_name(), record))
+    else:
+      get_logger().warning("sample {} record not found".format(stream_complete_push_url.get_name()))
+  except Exception as e:
+    get_logger().error("failed to retrieve sample {} record: {}".format(stream_complete_push_url.get_name(), e))
+    raise e
+
+
+##
 ## >>================================ main method ===============================>>
 ##
 if __name__ == "__main__":
@@ -3977,3 +4178,16 @@ if __name__ == "__main__":
   test_drop_stream_candidate_resolution_table(db)
   test_check_stream_candidate_resolution_exists(db)
   """
+  ##
+  ## stream complete push url
+  ##
+  test_create_stream_complete_push_url_table(db)
+  test_check_stream_complete_push_url_exists(db)
+  test_insert_stream_complete_push_url_record(db)
+  test_get_stream_complete_push_url_record(db)
+  test_update_stream_complete_push_url_record(db)
+  test_get_stream_complete_push_url_record(db)
+  test_delete_stream_complete_push_url_record(db)
+  test_get_stream_complete_push_url_record(db)
+  test_drop_stream_complete_push_url_table(db)
+  test_check_stream_complete_push_url_exists(db)
