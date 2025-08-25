15ddc64c87a6f816118e03b383fa7a1a6f12dbac
WangYan-Good <987639823@qq.com>
2025-08-25 23:09:34 +0800

    [feature][implementation] add live_core_sdk_pull_flv_data table and unit test

--- backend/src/unit_test/database/table/stream.py
+++ backend/src/unit_test/database/table/stream.py
@@ -13,7 +13,8 @@ from backend.src.database.table.stream                                import Liv
                                                                              StreamCandidateResolutionTable, \
                                                                              StreamCompletePushUrlTable, \
                                                                              LiveCoreSdkDataTable, \
-                                                                             LiveCoreSdkPullDataTable
+                                                                             LiveCoreSdkPullDataTable, \
+                                                                             LiveCoreSdkPullFlvDataTable
 from backend.src.base.log                                             import get_logger
 
 ##
@@ -790,7 +791,7 @@ def test_get_live_core_sdk_data_record(db:SocialMediaStreamDataBase = None):
     raise e
 
 ##
-## >>================================ live core sdk data table test method ===============================>>
+## >>================================ live core sdk pull data table test method ===============================>>
 ##
 
 def test_create_live_core_sdk_pull_data_table(db:SocialMediaStreamDataBase = None):
@@ -981,17 +982,214 @@ def test_get_live_core_sdk_pull_data_record(db:SocialMediaStreamDataBase = None)
     raise e
 
 ##
+## >>================================ live core sdk pull flv data table test method ===============================>>
+##
+
+def test_create_live_core_sdk_pull_flv_data_table(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if db is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table
+  ##
+  live_core_sdk_pull_flv_data_table = LiveCoreSdkPullFlvDataTable(db_instance=db)
+  live_core_sdk_pull_flv_data_table.create()
+  return
+
+##
+## test: drop table
+##
+def test_drop_live_core_sdk_pull_flv_data_table(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## drop table
+  ##
+  live_core_sdk_pull_flv_data_table = LiveCoreSdkPullFlvDataTable(db_instance=db)
+  live_core_sdk_pull_flv_data_table.drop()
+  return
+
+##
+## test: check if table exists
+##
+def test_check_live_core_sdk_pull_flv_data_table_exists(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+
+  live_core_sdk_pull_flv_data_table = LiveCoreSdkPullFlvDataTable(db)
+
+  ##
+  ## check if table exists
+  ##
+  if db.is_table_exist(live_core_sdk_pull_flv_data_table.get_name()):
+    get_logger().info("{} table exists!".format(live_core_sdk_pull_flv_data_table.get_name()))
+  else:
+    get_logger().info("{} table not exists!".format(live_core_sdk_pull_flv_data_table.get_name()))
+  return
+
+##
+## test: insert record
+##
+def test_insert_live_core_sdk_pull_flv_data_record(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table if not exists
+  ##
+  live_core_sdk_pull_flv_data = LiveCoreSdkPullFlvDataTable(db_instance=db)
+
+  ##
+  ## insert a sample record
+  ##
+  sample_record = {
+    'now': dat.fromtimestamp(1740301577026/1000.0),
+    'platform': 'douyin',
+    'room_id': '7411524533301119798',
+    'Flv_index': 0
+  }
+  
+  try:
+    live_core_sdk_pull_flv_data.insert_record(sample_record)
+    get_logger().info("sample record inserted successfully")
+  except Exception as e:
+    get_logger().error("failed to insert sample record: {}".format(e))
+    raise e
+
+##
+## test: delete record
+##
+def test_delete_live_core_sdk_pull_flv_data_record(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table if not exists
+  ##
+  live_core_sdk_pull_flv_data = LiveCoreSdkPullFlvDataTable(db_instance=db)
+
+  ##
+  ## delete a sample record
+  ##
+  sample_record = {
+    'now': dat.fromtimestamp(1740301577026/1000.0),
+    'platform': 'douyin',
+    'room_id': '7411524533301119798',
+    'Flv_index': 0
+  }
+  
+  try:
+    live_core_sdk_pull_flv_data.delete_record(sample_record)
+    get_logger().info("sample record deleted successfully")
+  except Exception as e:
+    get_logger().error("failed to delete sample record: {}".format(e))
+    raise e
+
+##
+## test: update record
+##
+def test_update_live_core_sdk_pull_flv_data_record(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table if not exists
+  ##
+  live_core_sdk_pull_flv_data = LiveCoreSdkPullFlvDataTable(db_instance=db)
+  
+  ##
+  ## update a sample record
+  ##
+  sample_record = {
+    'now': dat.fromtimestamp(1740301577026/1000.0),
+    'platform': 'douyin',
+    'room_id': '7411524533301119798',
+    'Flv_index': 0,
+    'Flv': '123456789'
+  }
+  
+  try:
+    live_core_sdk_pull_flv_data.update_record(sample_record)
+    get_logger().info("sample record updated successfully")
+  except Exception as e:
+    get_logger().error("failed to update sample record: {}".format(e))
+    raise e
+
+##
+## test: get record
+## 
+def test_get_live_core_sdk_pull_flv_data_record(db:SocialMediaStreamDataBase = None):
+  ##
+  ## check if database instance is valid
+  ##
+  if db is None:
+    get_logger().error("database instance is None, please provide a valid SocialMediaStreamDataBase instance")
+    raise ValueError
+  
+  ##
+  ## create table if not exist
+  ##
+  live_core_sdk_pull_flv_data = LiveCoreSdkPullFlvDataTable(db_instance=db)
+
+  ##
+  ## get a sample record
+  ##
+  sample_record = {
+    'now': dat.fromtimestamp(1740301577026/1000.0),
+    'platform': 'douyin',
+    'room_id': '7411524533301119798',
+    'Flv_index': 0
+  }
+  
+  try:
+    record = live_core_sdk_pull_flv_data.get_record(sample_record)
+    if record:
+      get_logger().info("sample {} record retrieved successfully: \n\t{}".format(live_core_sdk_pull_flv_data.get_name(), record))
+    else:
+      get_logger().warning("sample {} record not found".format(live_core_sdk_pull_flv_data.get_name()))
+  except Exception as e:
+    get_logger().error("failed to retrieve sample {} record: {}".format(live_core_sdk_pull_flv_data.get_name(), e))
+    raise e
+
+##
 ## >>================================ main method ===============================>>
 ##
 if __name__ == "__main__":
   db = SocialMediaStreamDataBase(host='192.168.1.12', user='wangyan', passwd='wuyu1998', database='social_media_stream_downloader')
   """
   test_create_live_stream_table(db)
+  test_check_live_stream_table_exists(db)
   test_insert_live_stream_record(db)
+  test_get_live_stream_record(db)
   test_update_live_stream_record(db)
   test_get_live_stream_record(db)
   test_delete_live_stream_record(db)
-  test_check_live_stream_table_exists(db)
+  test_get_live_stream_record(db)
   test_drop_live_stream_table(db)
   test_check_live_stream_table_exists(db)
 
@@ -999,11 +1197,13 @@ if __name__ == "__main__":
   ## stream candidate resolution
   ##
   test_create_stream_candidate_resolution_table(db)
+  test_check_stream_candidate_resolution_table_exists(db)
   test_insert_stream_candidate_resolution_record(db)
+  test_get_stream_candidate_resolution_record(db)
   test_update_stream_candidate_resolution_record(db)
   test_get_stream_candidate_resolution_record(db)
   test_delete_stream_candidate_resolution_record(db)
-  test_check_stream_candidate_resolution_table_exists(db)
+  test_get_stream_candidate_resolution_record(db)
   test_drop_stream_candidate_resolution_table(db)
   test_check_stream_candidate_resolution_table_exists(db)
 
@@ -1011,11 +1211,13 @@ if __name__ == "__main__":
   ## stream complete push url table
   ##
   test_create_stream_complete_push_url_table(db)
+  test_check_stream_complete_push_url_table_exists(db)
   test_insert_stream_complete_push_url_record(db)
+  test_get_stream_complete_push_url_record(db)
   test_update_stream_complete_push_url_record(db)
   test_get_stream_complete_push_url_record(db)
   test_delete_stream_complete_push_url_record(db)
-  test_check_stream_complete_push_url_table_exists(db)
+  test_get_stream_complete_push_url_record(db)
   test_drop_stream_complete_push_url_table(db)
   test_check_stream_complete_push_url_table_exists(db)
 
@@ -1023,11 +1225,13 @@ if __name__ == "__main__":
   ## live core sdk data table
   ##
   test_create_live_core_sdk_data_table(db)
+  test_check_live_core_sdk_data_table_exists(db)
   test_insert_live_core_sdk_data_record(db)
+  test_get_live_core_sdk_data_record(db)
   test_update_live_core_sdk_data_record(db)
   test_get_live_core_sdk_data_record(db)
   test_delete_live_core_sdk_data_record(db)
-  test_check_live_core_sdk_data_table_exists(db)
+  test_get_live_core_sdk_data_record(db)
   test_drop_live_core_sdk_data_table(db)
   test_check_live_core_sdk_data_table_exists(db)
 
@@ -1035,11 +1239,27 @@ if __name__ == "__main__":
   ## live core sdk pull data table
   ##
   test_create_live_core_sdk_pull_data_table(db)
+  test_check_live_core_sdk_pull_data_table_exists(db)
   test_insert_live_core_sdk_pull_data_record(db)
+  test_get_live_core_sdk_pull_data_record(db)
   test_update_live_core_sdk_pull_data_record(db)
   test_get_live_core_sdk_pull_data_record(db)
   test_delete_live_core_sdk_pull_data_record(db)
-  test_check_live_core_sdk_pull_data_table_exists(db)
+  test_get_live_core_sdk_pull_data_record(db)
   test_drop_live_core_sdk_pull_data_table(db)
   test_check_live_core_sdk_pull_data_table_exists(db)
+
+  ##
+  ## live core sdk pull flv data table
+  ##
+  test_create_live_core_sdk_pull_flv_data_table(db)
+  test_check_live_core_sdk_pull_flv_data_table_exists(db)
+  test_insert_live_core_sdk_pull_flv_data_record(db)
+  test_get_live_core_sdk_pull_flv_data_record(db)
+  test_update_live_core_sdk_pull_flv_data_record(db)
+  test_get_live_core_sdk_pull_flv_data_record(db)
+  test_delete_live_core_sdk_pull_flv_data_record(db)
+  test_get_live_core_sdk_pull_flv_data_record(db)
+  test_drop_live_core_sdk_pull_flv_data_table(db)
+  test_check_live_core_sdk_pull_flv_data_table_exists(db)
   """
